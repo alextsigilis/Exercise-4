706bd020737fa7e1bec07965d72614dd1edd56be
Αλέξ <alextsigilis@gmail.com>
2020-01-31 16:48:00 +0200

    findDegree seems to work

--- src/rcm.c
+++ src/rcm.c
@@ -2,11 +2,12 @@
 #include <limits.h>
 #include "rcm.h"
 
+#include <stdio.h>
 
 void findDegree( const size_t n, const double *A, unsigned int *deg ) {
 	
 	for( size_t v = 0; v < n; v++ ) {
-		deg[v] = 0;
+		deg[v] = -1; // Don't count itself.
 		for( size_t i = 0; i < n; i++) {
 			deg[v] += (Amat(v,i) != 0)? 1 : 0;
 		}
@@ -21,7 +22,11 @@ void rcm( const size_t n, const double *A, unsigned int *R ) {
 
 	findDegree( n, A, deg );
 
-	
+	for( size_t i = 0; i < n; i++ ) {
+
+		printf("deg( %d ) = %d \n", i, deg[i]);
+
+	}
 	
 
 }
